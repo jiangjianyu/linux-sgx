a743132c4f09f1182e277ad484756ba217fd54f7
jianyu <jianyu@hku.hk>
2020-12-30 17:11:28 +0800

    rsa

--- sdk/tlibcrypto/sgx_rsa3072.cpp
+++ sdk/tlibcrypto/sgx_rsa3072.cpp
@@ -31,7 +31,9 @@
 
 
 #include "sgx_ecc256_common.h"
+#include "ippcore.h"
 
+#define SHA_SIZE_BIT  256
 
 sgx_status_t sgx_rsa3072_sign(const uint8_t * p_data, 
     uint32_t data_size, 
@@ -211,4 +213,175 @@ sgx_status_t sgx_rsa3072_verify(const uint8_t *p_data,
     case ippStsBadArgErr: return SGX_ERROR_INVALID_PARAMETER;
     default: return SGX_ERROR_UNEXPECTED;
     }
+}
+
+IppsBigNumState* createBigNumState(int len, const Ipp32u* pData)  {
+    int size;
+    ippsBigNumGetSize(len, &size);
+    IppsBigNumState* pBN = (IppsBigNumState*) ippMalloc(size);;
+    ippsBigNumInit(len, pBN);
+    if (pData != NULL) {
+        ippsSet_BN(IppsBigNumPOS, len, pData, pBN);
+    }
+    return pBN;
+}
+
+extern "C" sgx_status_t sgx_rsa_keygen (int bitsRSA, IppsRSAPublicKeyState **pubkey, IppsRSAPrivateKeyState **privkey) {
+
+    IppStatus status;
+
+    // Security parameter specified for the
+    // Miller-Rabin test for probable primality.
+    int nTrials = 1;
+    // Number of bits of the exponent
+    int bitsExp = 24;
+
+    int sizeOfPrimeGen      = -1;
+    int sizeOfRandomGen     = -1;
+    int sizeOfPublicKey     = -1;
+    int sizeOfPrivateKey    = -1;
+    int sizeOfScratchBuffer = -1;
+
+    IppsPrimeState * primeGen           = NULL;
+    IppsPRNGState * randomGen           = NULL;
+    IppsRSAPublicKeyState * publicKey   = NULL;
+    IppsRSAPrivateKeyState * privateKey = NULL;
+    Ipp8u * scratchBuffer               = NULL;
+
+    Ipp32u E = 65537;
+    IppsBigNumState * pSrcPublicExp = createBigNumState (1, &E);
+    IppsBigNumState * pModulus      = createBigNumState (bitsRSA / 32, NULL);
+    IppsBigNumState * pPublicExp    = createBigNumState (bitsRSA / 32, NULL);
+    IppsBigNumState * pPrivateExp   = createBigNumState (bitsRSA / 32, NULL);
+
+    // Prime Number Generator
+    ippsPrimeGetSize(bitsRSA, &sizeOfPrimeGen);
+    primeGen = (IppsPrimeState *) ippMalloc(sizeOfPrimeGen);
+    ippsPrimeInit(bitsRSA, primeGen);
+
+    // Pseudo Random Generator (default settings)
+    ippsPRNGGetSize(&sizeOfRandomGen);
+    randomGen = (IppsPRNGState*) ippMalloc (sizeOfRandomGen);
+    ippsPRNGInit(160, randomGen);
+
+    // Initialize the Public Key State
+    ippsRSA_GetSizePublicKey(bitsRSA, bitsExp, &sizeOfPublicKey);
+    publicKey = (IppsRSAPublicKeyState *)ippMalloc(sizeOfPublicKey);
+    ippsRSA_InitPublicKey(bitsRSA, bitsExp, publicKey, sizeOfPublicKey);
+
+    // Initialize the Private Key State
+    int bitsP = (bitsRSA + 1) / 2;
+    int bitsQ = bitsRSA - bitsP;
+    ippsRSA_GetSizePrivateKeyType2(bitsRSA, bitsRSA, &sizeOfPrivateKey);
+    privateKey = (IppsRSAPrivateKeyState *) ippMalloc(sizeOfPrivateKey);
+    ippsRSA_InitPrivateKeyType2(bitsP, bitsQ, privateKey, sizeOfPrivateKey);
+
+    // Initialize scratch buffer
+    ippsRSA_GetBufferSizePrivateKey(&sizeOfScratchBuffer, privateKey);
+    scratchBuffer = (Ipp8u *) ippMalloc (sizeOfScratchBuffer);
+
+    status = ippsRSA_GenerateKeys(
+        pSrcPublicExp,
+        pModulus, pPublicExp, pPrivateExp,
+        privateKey, scratchBuffer, nTrials,
+        primeGen, ippsPRNGen, randomGen
+    );
+
+    ippsRSA_SetPublicKey(pModulus, pPublicExp, publicKey);
+
+    ippFree(pSrcPublicExp);
+    ippFree(pModulus);
+    ippFree(pPublicExp);
+    ippFree(pPrivateExp);
+    ippFree(primeGen);
+    ippFree(randomGen);
+    // ippFree(publicKey);
+    // ippFree(privateKey);
+    ippFree(scratchBuffer);
+
+    *pubkey = publicKey;
+    *privkey = privateKey;
+
+    if (status == ippStsNoErr)
+        return SGX_SUCCESS;
+    else
+        return SGX_ERROR_INVALID_PARAMETER;
+}
+
+//This function will do the rsa oaep encryption with input src[0:src_len] and put the output to buffer dst
+//The function will assume that buffer src_len is no more than PVE_RSAOAEP_ENCRYPT_MAXLEN and the buffer size of dst is at least PVE_RSA_KEY_BITS
+extern "C" sgx_status_t sgx_rsa_oaep_encrypt(const uint8_t *src, uint32_t src_len, const IppsRSAPublicKeyState *rsa, uint8_t* dst)
+{
+    const int hashsize = SHA_SIZE_BIT;
+    Ipp8u seeds[hashsize];
+    IppStatus status = ippStsNoErr;
+    sgx_status_t ret = SGX_SUCCESS;
+    uint8_t* pub_key_buffer = NULL;
+    int pub_key_size;
+
+    ret = sgx_read_rand(seeds, hashsize);
+    if(SGX_SUCCESS != ret){
+        goto ret_point;
+    }
+
+    if((status = ippsRSA_GetBufferSizePublicKey(&pub_key_size, rsa)) != ippStsNoErr)
+    {
+        ret = SGX_ERROR_INVALID_PARAMETER;
+        goto ret_point;
+    }
+
+    //allocate temporary buffer
+    pub_key_buffer = (uint8_t*)malloc(pub_key_size);
+    if(pub_key_buffer == NULL)
+    {
+        ret = SGX_ERROR_OUT_OF_MEMORY;
+        goto ret_point;
+    }
+
+    if((status = ippsRSAEncrypt_OAEP(src, src_len,
+                                        NULL, 0, seeds,
+                                        dst, rsa, IPP_ALG_HASH_SHA256, pub_key_buffer)) != ippStsNoErr)
+    {
+        ret = (sgx_status_t)status;
+        goto ret_point;
+    }
+
+ret_point:
+    if(pub_key_buffer)
+        free(pub_key_buffer);
+    return ret;
+}
+
+extern "C" sgx_status_t sgx_rsa_oaep_decrypt(const uint8_t *src, int* dst_len, const IppsRSAPrivateKeyState *rsa, uint8_t* dst)
+{
+    IppStatus status = ippStsNoErr;
+    sgx_status_t ret = SGX_SUCCESS;
+    uint8_t* priv_key_buffer = NULL;
+    int pub_key_size;
+
+    if((status = ippsRSA_GetBufferSizePrivateKey(&pub_key_size, rsa)) != ippStsNoErr)
+    {
+        ret = SGX_ERROR_INVALID_PARAMETER;
+        goto ret_point;
+    }
+
+    //allocate temporary buffer
+    priv_key_buffer = (uint8_t*)malloc(pub_key_size);
+    if(priv_key_buffer == NULL)
+    {
+        ret = SGX_ERROR_OUT_OF_MEMORY;
+        goto ret_point;
+    }
+
+    if((status = ippsRSADecrypt_OAEP(src, NULL, 0,
+                                        dst, dst_len, rsa, IPP_ALG_HASH_SHA256, priv_key_buffer)) != ippStsNoErr)
+    {
+        ret = SGX_ERROR_INVALID_PARAMETER;
+        goto ret_point;
+    }
+
+ret_point:
+    if(priv_key_buffer)
+        free(priv_key_buffer);
+    return ret;
 }
